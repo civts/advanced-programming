e9e37ad3e4dac3b327cb57296fcc418e895bc97b
civts <giacomo.zanolli@studenti.unitn.it>
2022-12-03 15:12:12 +0100

    Add read from file constructor 🐜

--- market_sol/src/market_meta.rs
+++ market_sol/src/market_meta.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
-use unitn_market_2022::good::good_kind::GoodKind;
 use crate::good_lock_meta::GoodLockMeta;
+use std::{collections::HashMap, path::Path};
+use unitn_market_2022::good::good_kind::GoodKind;
 
 #[derive(Debug)]
 pub struct MarketMeta {
@@ -9,7 +9,8 @@ pub struct MarketMeta {
     // Key is token
     pub locked_sells: HashMap<String, GoodLockMeta>,
     pub min_bid: HashMap<GoodKind, f32>,
-    pub current_day: u32
+    pub current_day: u32,
+    pub file_path: Option<String>,
 }
 
 impl MarketMeta {
@@ -18,7 +19,19 @@ impl MarketMeta {
             locked_buys: Default::default(),
             locked_sells: Default::default(),
             min_bid: Default::default(), // todo: come up with min bid for each goods
-            current_day: 0u32
+            current_day: 0u32,
+            file_path: None,
         }
+    }
+
+    pub fn new_with_file(f: &Path) -> Self {
+        let file_str = f.to_str().unwrap();
+        return Self {
+            locked_buys: Default::default(),
+            locked_sells: Default::default(),
+            min_bid: Default::default(), // todo: come up with min bid for each goods
+            current_day: 0u32,
+            file_path: Some(String::from(file_str)),
+        };
     }
 }
