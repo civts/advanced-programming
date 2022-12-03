64d2b94c5e0cd7b3530a242cac3fcaa2937ca969
civts <giacomo.zanolli@studenti.unitn.it>
2022-12-03 20:41:12 +0100

    Add missing imports and members 🐡

--- market_sol/src/main.rs
+++ market_sol/src/main.rs
@@ -1,11 +1,10 @@
+mod banner;
+mod good_lock_meta;
 mod good_meta;
 mod market;
+mod market_meta;
 mod market_metadata;
 mod market_test;
-mod good_lock_meta;
-mod market_meta;
-
-extern crate unitn_market_2022;
 
 fn main() {
     // Create market
--- market_sol/src/market_meta.rs
+++ market_sol/src/market_meta.rs
@@ -36,6 +36,8 @@ impl MarketMeta {
             min_bid: Default::default(), // todo: come up with min bid for each goods
             current_day: 0u32,
             file_path: Some(String::from(file_str)),
+            num_of_sell_locks: 0,
+            num_of_buy_locks: 0,
         };
     }
 }
