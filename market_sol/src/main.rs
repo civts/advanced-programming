ff09d84297f207e605fdb846baf0124915b1827f
civts <giacomo.zanolli@studenti.unitn.it>
2022-12-06 02:33:24 +0100

    Showing all goods at once ⌛️

--- market_sol/src/main.rs
+++ market_sol/src/main.rs
@@ -15,59 +15,88 @@ mod lib;
 mod tests;
 
 fn main() {
-    // Create market
     let sum = 10000.0;
-    let market_ref = SOLMarket::new_with_quantities_and_meta(sum, sum, sum, sum, MarketMeta::new());
-
     let days = 3650;
     let interval = 1;
-    // Simulate time passing
-    let mut prices: Vec<f32> = Vec::new();
-    let mut min = f32::MAX;
-    let mut max = f32::MIN;
-    let gk = GoodKind::YEN;
-    for _ in 0..days {
-        let price = market_ref.borrow().get_buy_price(gk, 1.0).unwrap();
-        prices.push(price);
-        min = f32::min(min, price);
-        max = f32::max(max, price);
-        for _ in 0..interval {
-            wait_one_day!(market_ref);
-        }
-    }
+
     // Config chart
     let date_now = chrono::offset::Local::now();
-    let name = format!("./test_{gk}_{:?}.svg", date_now);
-    let drawing_area = SVGBackend::new(name.as_str(), (1920, 1080)).into_drawing_area();
-    let (top, bottom) = drawing_area.split_vertically(1000);
-    top.fill(&WHITE).unwrap();
-    let mut drawing_context = ChartBuilder::on(&top)
-        .set_label_area_size(LabelAreaPosition::Left, 40.0)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40.0)
-        .set_label_area_size(LabelAreaPosition::Right, 40.0)
-        .set_label_area_size(LabelAreaPosition::Top, 40.0)
-        .caption(format!("SOL Market going, {gk}"), ("sans-serif", 40.0))
-        .build_cartesian_2d(0.0..(days as f32), min..max)
-        .unwrap();
+    let name = format!("./test_{:?}.svg", date_now);
+    let margin_bottom = 80;
+    let y = 1080;
+    let x = 1920;
+    let drawing_area = SVGBackend::new(name.as_str(), (x, y)).into_drawing_area();
+    drawing_area.fill(&WHITE).unwrap();
+    let (top, bottom) = drawing_area.split_vertically(y - margin_bottom);
+    let (usd, t2) = top.split_vertically((y - margin_bottom) / 2);
+    let half = x as f32 / 2.0;
+    let (t3, t4) = t2.split_horizontally(half);
+    let (yen, _) = t3.split_horizontally(half * 0.98);
+    let (_, yuan) = t4.split_horizontally(half * 0.02);
 
-    drawing_context.configure_mesh().draw().unwrap();
+    for gk in [GoodKind::USD, GoodKind::YEN, GoodKind::YUAN] {
+        //Generate data
+        let market_ref =
+            SOLMarket::new_with_quantities_and_meta(sum, sum, sum, sum, MarketMeta::new());
+        let mut prices: Vec<f32> = Vec::new();
+        let mut min = f32::MAX;
+        let mut max = f32::MIN;
+        for _ in 0..days {
+            let price = market_ref.borrow().get_buy_price(gk, 1.0).unwrap();
+            prices.push(price);
+            min = f32::min(min, price);
+            max = f32::max(max, price);
+            for _ in 0..interval {
+                wait_one_day!(market_ref);
+            }
+        }
 
-    let price_points = LineSeries::new(
-        prices.into_iter().enumerate().map(|t| (t.0 as f32, t.1)),
-        &BLUE,
-    );
-    drawing_context.draw_series(price_points).unwrap();
+        let area = match gk {
+            GoodKind::EUR => panic!("eur exchange is always 1"),
+            GoodKind::YEN => yen.clone(),
+            GoodKind::USD => usd.clone(),
+            GoodKind::YUAN => yuan.clone(),
+        };
+        let dim = match gk {
+            GoodKind::EUR => panic!("eur exchange is always 1"),
+            GoodKind::YEN => 20.0,
+            GoodKind::USD => 40.0,
+            GoodKind::YUAN => 20.0,
+        };
 
-    let meta = &market_ref.borrow().meta;
-    let binding = meta.price_state.borrow();
-    let mut past_seasons = binding.past_seasons.get(&gk).unwrap().clone();
-    if let Some(current_season) = binding.seasons.get(&gk) {
-        past_seasons.push(*current_season);
+        let mut drawing_context = ChartBuilder::on(&area)
+            .set_label_area_size(LabelAreaPosition::Left, dim)
+            .set_label_area_size(LabelAreaPosition::Bottom, dim)
+            .set_label_area_size(LabelAreaPosition::Right, dim)
+            .set_label_area_size(LabelAreaPosition::Top, dim)
+            .caption(format!("SOL Market, {gk}"), ("sans-serif", dim))
+            .build_cartesian_2d(0.0..(days as f32), min..max)
+            .unwrap();
+
+        drawing_context.configure_mesh().draw().unwrap();
+
+        let price_points = LineSeries::new(
+            prices.into_iter().enumerate().map(|t| (t.0 as f32, t.1)),
+            &BLUE,
+        );
+        drawing_context.draw_series(price_points).unwrap();
+
+        let meta = &market_ref.borrow().meta;
+        let binding = meta.price_state.borrow();
+        let hash_map = &binding.past_seasons;
+        let mut past_seasons = hash_map.get(&gk).unwrap().clone();
+        if let Some(current_season) = binding.seasons.get(&gk) {
+            past_seasons.push(*current_season);
+        }
+        let season_marks = past_seasons.iter().map(|s| {
+            Circle::new(
+                (s.starting_day as f32, s.starting_price),
+                dim / 10.0,
+                RED.filled(),
+            )
+        });
+        drawing_context.draw_series(season_marks).unwrap();
     }
-    let season_marks = past_seasons
-        .iter()
-        .map(|s| Circle::new((s.starting_day as f32, s.starting_price), 4.0, RED.filled()));
-    drawing_context.draw_series(season_marks).unwrap();
 
     let txt = format!(
         "max season length: {}, min season length {}\nmin variation in season {}\nmin noise clamp {}, max noise clamp {}",
