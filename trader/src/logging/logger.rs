9bfd7286003932f0400588af52127fed96817e23
DTS1234 <adas.kazmierczak@gmail.com>
2022-12-23 18:57:24 +0100

    Add log trade for txt strategy

--- trader/src/logging/logger.rs
+++ trader/src/logging/logger.rs
@@ -14,7 +14,20 @@ impl Logger {
         amount: f32,
         operationType: String,
     ) {
-        self.strategy.log_lock(token, goodKind, quant, amount, operationType)
+        self.strategy
+            .log_lock(token, goodKind, quant, amount, operationType)
+    }
+
+    pub fn log_trade(
+        &self,
+        operation_type: String,
+        token: String,
+        quant: f32,
+        value: f32,
+        good_kind: String,
+    ) {
+        self.strategy
+            .log_trade(operation_type, token, quant, value, good_kind)
     }
 }
 
@@ -27,7 +40,14 @@ pub trait LoggingStrategy {
         amount: f32,
         operation_type: String,
     );
-    fn log_trade(&self);
+    fn log_trade(
+        &self,
+        operation_type: String,
+        token: String,
+        quant: f32,
+        value: f32,
+        good_kind: String,
+    );
 }
 
 pub struct PdfStrategy {}
@@ -37,8 +57,14 @@ pub struct TxtStrategy {}
 impl TxtStrategy {}
 
 impl LoggingStrategy for TxtStrategy {
-    
-    fn log_lock(&self, token: String, good_kind: String, quant: f32, amount: f32, operation_type: String) {
+    fn log_lock(
+        &self,
+        token: String,
+        good_kind: String,
+        quant: f32,
+        amount: f32,
+        operation_type: String,
+    ) {
         let filename = format!("log_trader.txt");
         let mut file = OpenOptions::new()
             .append(true)
@@ -46,16 +72,28 @@ impl LoggingStrategy for TxtStrategy {
             .open(filename)
             .unwrap();
 
-        let time = chrono::Local::now()
-            .format("%y:%m:%d:%H:%M:%S:%3f")
-            .to_string();
-
         if let Err(e) = writeln!(file, "LOG INFO - LOCK_{operation_type} token: {token}, goodKind: {good_kind}, quantity: {quant}, amount: {amount}") {
             eprintln!("Error while writing to file {}", e);
         }
     }
 
-    fn log_trade(&self) {
-        todo!()
+    fn log_trade(
+        &self,
+        operation_type: String,
+        token: String,
+        quant: f32,
+        value: f32,
+        good_kind: String,
+    ) {
+        let filename = format!("log_trader.txt");
+        let mut file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(filename)
+            .unwrap();
+
+        if let Err(e) = writeln!(file, "LOG INFO - {operation_type} token: {token}, quantity: {quant}, value: {value}, goodKind: {good_kind}") {
+            eprintln!("Error while writing to file {}", e);
+        }
     }
 }
--- trader/src/tests/logging_test.rs
+++ trader/src/tests/logging_test.rs
@@ -9,11 +9,46 @@ fn should_log_lock_in_txt() {
         strategy: Box::new(TxtStrategy {}),
     };
     // when
-    logger.log_lock(String::from("token"), String::from("EUR"), 30.0, 45.0, String::from("BUY"));
+    logger.log_lock(
+        String::from("token"),
+        String::from("EUR"),
+        30.0,
+        45.0,
+        String::from("BUY"),
+    );
     // then
     let contents =
         fs::read_to_string("log_trader.txt").expect("Should have been able to read the file");
-    assert_eq!("LOG INFO - LOCK_BUY token: token, goodKind: EUR, quantity: 30, amount: 45\n".trim(), contents.trim());
+    assert_eq!(
+        "LOG INFO - LOCK_BUY token: token, goodKind: EUR, quantity: 30, amount: 45\n".trim(),
+        contents.trim()
+    );
+
+    // after
+    fs::remove_file("log_trader.txt");
+}
+
+#[test]
+fn should_log_trade_in_txt() {
+    // given
+    let logger: Logger = Logger {
+        strategy: Box::new(TxtStrategy {}),
+    };
+    // when
+    logger.log_trade(
+        String::from("BUY"),
+        String::from("token"),
+        30.0,
+        45.0,
+        String::from("EUR"),
+    );
+    // then
+    let contents =
+        fs::read_to_string("log_trader.txt").expect("Should have been able to read the file");
+    assert_eq!(
+        "LOG INFO - BUY token: token, quantity: 30, value: 45, goodKind: EUR\n".trim(),
+        contents.trim()
+    );
 
     // after
     fs::remove_file("log_trader.txt");
