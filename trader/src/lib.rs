637081235ec323d24944245e94b345e52ee24b1e
DTS1234 <adas.kazmierczak@gmail.com>
2022-12-23 18:40:35 +0100

    Create trader logging infrastracture and setup the project

--- trader/src/lib.rs
+++ trader/src/lib.rs
@@ -2,13 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod logging;
+pub mod trader;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use self::logging::*;
+
+#[cfg(test)]
+mod tests;
---
+++ trader/src/logging/logger.rs
@@ -0,0 +1,63 @@
+use std::fs::OpenOptions;
+use std::io::Write;
+
+pub struct Logger {
+    pub strategy: Box<dyn LoggingStrategy>,
+}
+
+impl Logger {
+    pub fn log_lock(
+        &self,
+        token: String,
+        goodKind: String,
+        quant: f32,
+        amount: f32,
+        operationType: String,
+    ) {
+        self.strategy.log_lock(token, goodKind, quant, amount, operationType)
+    }
+}
+
+pub trait LoggingStrategy {
+    fn log_lock(
+        &self,
+        token: String,
+        good_kind: String,
+        quant: f32,
+        amount: f32,
+        operation_type: String,
+    );
+    fn log_trade(&self);
+}
+
+pub struct PdfStrategy {}
+
+pub struct TxtStrategy {}
+
+impl TxtStrategy {}
+
+impl LoggingStrategy for TxtStrategy {
+    
+    fn log_lock(&self, token: String, good_kind: String, quant: f32, amount: f32, operation_type: String) {
+        let filename = format!("log_trader.txt");
+        let mut file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(filename)
+            .unwrap();
+
+        let time = chrono::Local::now()
+            .format("%y:%m:%d:%H:%M:%S:%3f")
+            .to_string();
+
+        
+
+        if let Err(e) = writeln!(file, "LOG LOCK_{operation_type}: token: {token}, goodKind: {good_kind}, quantity: {quant}, amount: {amount}") {
+            eprintln!("Error while writing to file {}", e);
+        }
+    }
+
+    fn log_trade(&self) {
+        todo!()
+    }
+}
