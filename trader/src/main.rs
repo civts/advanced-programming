7258f5871a46403163251b61bb23bbb8239dc617
ImGilbes <gianlucasax@hotmail.it>
2023-01-14 16:32:55 +0100

    added rate histories

--- trader/src/main.rs
+++ trader/src/main.rs
@@ -3,10 +3,15 @@
 //take the markets out of the trader into a separate playground object?
 //maybe too complicated
 
+use std::{borrow::Borrow, collections::HashMap};
+
+use std::{thread, time};
 use trader::trader::SOLTrader;
+use unitn_market_2022::{good::good_kind::GoodKind, wait_one_day};
 
 pub fn main() {
-    println!("ciao");
+    let mut history_buy: Vec<HashMap<String, HashMap<GoodKind, f32>>> = Vec::new();
+    let mut history_sell: Vec<HashMap<String, HashMap<GoodKind, f32>>> = Vec::new();
 
     let generic_init_quantity = 1000.0;
     let mut trader = SOLTrader::new_with_quantities(generic_init_quantity, 0.0, 0.0, 0.0);
@@ -15,10 +20,28 @@ pub fn main() {
     trader.show_all_self_quantities();
 
     trader.show_all_market_info();
+
+    history_buy.push(trader.get_all_current_buy_rates());
+    history_sell.push(trader.get_all_current_sell_rates());
+
+    //trader main loop, each loop a different trade
+    for _ in 0..3 {
+        make_trade(&trader);
+
+        show_delta();
+
+        history_buy.push(trader.get_all_current_buy_rates());
+        history_sell.push(trader.get_all_current_sell_rates());
+
+        let d = get_delta_last_day(history_buy.clone()).unwrap();
+        println!("\n{:?}", d);
+
+        // thread::sleep(time::Duration::from_secs(5))
+    }
 }
 
-
-pub fn make_trade(){
+//here we can implement the stategy of the trader
+pub fn make_trade(trader: &SOLTrader) {
     //select next trade partner
 
     //select next good
@@ -26,8 +49,36 @@ pub fn make_trade(){
     //select next quantity
 
     //trade!
+    trader.all_wait_one_day();
 }
 
-fn show_delta(){
+//tested: delta is zero qith no trades
+fn get_delta_last_day(
+    history: Vec<HashMap<String, HashMap<GoodKind, f32>>>,
+) -> Option<HashMap<String, HashMap<GoodKind, f32>>> {
+    if history.len() >= 2 {
+        // if at least one day has passed
 
+        let mut delta: HashMap<String, HashMap<GoodKind, f32>> = HashMap::new();
+
+        let yesterday = history[history.len() - 1].clone();
+
+        for (name, abc) in history[history.len() - 2].clone() {
+            let mut tmp: HashMap<GoodKind, f32> = HashMap::new();
+
+            for (good, rate) in abc {
+                // let acbabca = rate-yesterday[&name][&good];
+                tmp.insert(good.clone(), rate - yesterday[&name][&good]);
+            }
+
+            delta.insert(name.clone(), tmp);
+        }
+
+        return Some(delta);
+    }
+    None
 }
+
+fn show_delta() {}
+
+fn print_history() {}
