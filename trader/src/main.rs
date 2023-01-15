7c82260cc65f0412d8da6ac6a081bee516d91818
ImGilbes <gianlucasax@hotmail.it>
2023-01-15 15:03:35 +0100

    new basic strategy: best trade based on deltas

--- trader/src/main.rs
+++ trader/src/main.rs
@@ -7,6 +7,7 @@ use std::{borrow::Borrow, collections::HashMap};
 
 use std::{thread, time};
 use trader::trader::SOLTrader;
+use unitn_market_2022::good::consts::DEFAULT_GOOD_KIND;
 use unitn_market_2022::{good::good_kind::GoodKind, wait_one_day};
 
 //NOTES
@@ -14,12 +15,10 @@ use unitn_market_2022::{good::good_kind::GoodKind, wait_one_day};
 // MAIN manages history of prices, history display, next choices (can puut into separate objects later)
 //can define type history = ...
 
-pub fn main() {
-    let mut history_buy: Vec<HashMap<String, HashMap<GoodKind, f32>>> = Vec::new();
-    let mut history_sell: Vec<HashMap<String, HashMap<GoodKind, f32>>> = Vec::new();
-    let mut history_delta: Vec<HashMap<String, HashMap<GoodKind, f32>>> = Vec::new();
+type History = Vec<HashMap<String, HashMap<GoodKind, f32>>>;
 
-    let generic_init_quantity = 1000.0;
+pub fn main() {
+    let generic_init_quantity = 10000.0;
     let mut trader = SOLTrader::new_with_quantities(
         generic_init_quantity,
         generic_init_quantity,
@@ -32,21 +31,38 @@ pub fn main() {
 
     trader.show_all_market_info();
 
+    //trader main loop, each loop a different trade
+    // basic_all_random_strategy(&mut trader, 6);
+    // do_nothing_strategy(&mut trader, 6);
+    basic_best_trade_strategy(&mut trader, 6);
+}
+
+fn basic_all_random_strategy(trader: &mut SOLTrader, iterations: u32) {
+    let mut history_buy: History = Vec::new();
+    let mut history_sell: History = Vec::new();
+    let mut buy_deltas: History = Vec::new();
+    let mut sell_deltas: History = Vec::new();
+
     history_buy.push(trader.get_all_current_buy_rates());
     history_sell.push(trader.get_all_current_sell_rates());
 
-    //trader main loop, each loop a different trade
-    for _ in 0..6 {
-        make_trade_all_random(&mut trader);
+    for _ in 0..iterations {
+        make_trade_all_random(trader);
 
         show_delta();
 
         history_buy.push(trader.get_all_current_buy_rates());
         history_sell.push(trader.get_all_current_sell_rates());
-        history_delta.push(get_delta_last_day(history_buy.clone()).unwrap());
+        buy_deltas.push(get_delta_last_day(history_buy.clone()).unwrap());
+        sell_deltas.push(get_delta_last_day(history_sell.clone()).unwrap());
 
-        // let d = get_delta_last_day(history_buy.clone()).unwrap();
-        println!("\n{:?}", history_delta[history_delta.len() - 1]);
+        println!("\nBUY DELTAS\n{:?}", buy_deltas[buy_deltas.len() - 1]);
+        println!("\nSELL DELTAS\n{:?}", sell_deltas[sell_deltas.len() - 1]);
+
+        let (a, b) = get_best_buy_delta(&buy_deltas);
+        println!("\n today's best BUY delta is {} {}", a, b);
+        let (a, b) = get_best_sell_delta(&sell_deltas);
+        println!("\n today's best SELL delta is {} {}", a, b);
 
         // thread::sleep(time::Duration::from_secs(5))
         trader.show_all_self_quantities();
@@ -73,6 +89,95 @@ pub fn make_trade_all_random(trader: &mut SOLTrader) {
         trader.buy_from_market(name.to_owned(), kind, qty);
     } else {
         trader.sell_to_market(name.to_owned(), kind, qty);
+    }
+}
+
+//first makes one random trade, than looks at the deltas and starts making the best trades possible
+//best trade means either
+///buy (market,goodkind) with the lowest delta (bargain)
+//or sell (market,goodkind) with the highest delta (amke the most out of what you bought)
+//the quantities are still random
+fn basic_best_trade_strategy(trader: &mut SOLTrader, iterations: u32) {
+    let mut history_buy: History = Vec::new();
+    let mut history_sell: History = Vec::new();
+    let mut buy_deltas: History = Vec::new();
+    let mut sell_deltas: History = Vec::new();
+
+    history_buy.push(trader.get_all_current_buy_rates());
+    history_sell.push(trader.get_all_current_sell_rates());
+
+    make_trade_all_random(trader);
+    history_buy.push(trader.get_all_current_buy_rates());
+    history_sell.push(trader.get_all_current_sell_rates());
+    buy_deltas.push(get_delta_last_day(history_buy.clone()).unwrap());
+    sell_deltas.push(get_delta_last_day(history_sell.clone()).unwrap());
+
+    for _ in 0..iterations - 1 {
+        make_best_trade(trader, &buy_deltas, &sell_deltas);
+
+        show_delta();
+
+        history_buy.push(trader.get_all_current_buy_rates());
+        history_sell.push(trader.get_all_current_sell_rates());
+        buy_deltas.push(get_delta_last_day(history_buy.clone()).unwrap());
+        sell_deltas.push(get_delta_last_day(history_sell.clone()).unwrap());
+
+        println!("\nBUY DELTAS\n{:?}", buy_deltas[buy_deltas.len() - 1]);
+        println!("\nSELL DELTAS\n{:?}", sell_deltas[sell_deltas.len() - 1]);
+
+        let (a, b) = get_best_buy_delta(&buy_deltas);
+        println!("\n today's best BUY delta is {} {}", a, b);
+        let (a, b) = get_best_sell_delta(&sell_deltas);
+        println!("\n today's best SELL delta is {} {}", a, b);
+
+        // thread::sleep(time::Duration::from_secs(5))
+        trader.show_all_self_quantities();
+    }
+}
+
+fn make_best_trade(trader: &mut SOLTrader, buy_deltas: &History, sell_deltas: &History) {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+
+    let (kind_buy, name_buy) = get_best_buy_delta(buy_deltas);
+
+    let (kind_sell, name_sell) = get_best_sell_delta(sell_deltas);
+
+    //select next quantity
+    let qty = rng.gen_range(500..1000) as f32;
+    //trade!
+
+    //still selects the kind of trade randomly
+    if rng.gen_range(0..2) == 0 {
+        trader.buy_from_market(name_buy.to_owned(), kind_buy, qty);
+    } else {
+        trader.sell_to_market(name_sell.to_owned(), kind_sell, qty);
+    }
+}
+
+fn do_nothing_strategy(trader: &mut SOLTrader, iterations: u32) {
+    let mut history_buy: History = Vec::new();
+    let mut history_sell: History = Vec::new();
+    let mut buy_deltas: History = Vec::new();
+    let mut sell_deltas: History = Vec::new();
+
+    history_buy.push(trader.get_all_current_buy_rates());
+    history_sell.push(trader.get_all_current_sell_rates());
+
+    for _ in 0..iterations {
+        fake_trade(trader);
+
+        show_delta();
+
+        history_buy.push(trader.get_all_current_buy_rates());
+        history_sell.push(trader.get_all_current_sell_rates());
+
+        sell_deltas.push(get_delta_last_day(history_sell.clone()).unwrap());
+
+        println!("\n{:?}", sell_deltas[sell_deltas.len() - 1]);
+
+        // thread::sleep(time::Duration::from_secs(5))
+        trader.show_all_self_quantities();
     }
 }
 
@@ -110,10 +215,42 @@ fn show_delta() {}
 
 fn print_history() {}
 
-fn get_best_sell_delta() -> (GoodKind, String) {
-    todo!()
+fn get_best_sell_delta(h: &History) -> (GoodKind, String) {
+    let delta = &h[h.len() - 1];
+    let mut res_kind: GoodKind = GoodKind::USD;
+    let mut res_market: String = String::from("DogeMarket");
+    let mut max_found: f32 = delta["DogeMarket"][&GoodKind::USD];
+
+    for (market, map) in delta {
+        for (good, delta) in map {
+            if *good != DEFAULT_GOOD_KIND {
+                if *delta > max_found {
+                    res_kind = *good;
+                    res_market = market.clone();
+                    max_found = *delta;
+                }
+            }
+        }
+    }
+    (res_kind, res_market)
 }
 
-fn get_best_buy_delta() -> (GoodKind, String) {
-    todo!()
+fn get_best_buy_delta(h: &History) -> (GoodKind, String) {
+    let delta = &h[h.len() - 1];
+    let mut res_kind: GoodKind = GoodKind::USD;
+    let mut res_market: String = String::from("DogeMarket");
+    let mut min_found: f32 = delta["DogeMarket"][&GoodKind::USD];
+
+    for (market, map) in delta {
+        for (good, delta) in map {
+            if *good != DEFAULT_GOOD_KIND {
+                if *delta < min_found {
+                    res_kind = *good;
+                    res_market = market.clone();
+                    min_found = *delta;
+                }
+            }
+        }
+    }
+    (res_kind, res_market)
 }
