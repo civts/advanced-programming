fcf9549a6213bac315b3c48a42d173dd96af99bb
civts <giacomo.zanolli@studenti.unitn.it>
2023-02-06 10:12:40 +0100

    Update vscode and extensions 🧩

--- flake.nix
+++ flake.nix
@@ -40,9 +40,22 @@
                 jnoortheen.nix-ide
                 matklad.rust-analyzer
                 vadimcn.vscode-lldb
-                bungcip.better-toml
+                tamasfe.even-better-toml
+                #usernamehw.find-jump
                 esbenp.prettier-vscode
               ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                {
+                  name = "better-comments";
+                  publisher = "aaron-bond";
+                  version = "3.0.2";
+                  sha256 = "sha256-hQmA8PWjf2Nd60v5EAuqqD8LIEu7slrNs8luc3ePgZc=";
+                }
+                {
+                  name = "gitblame";
+                  publisher = "waderyan";
+                  version = "10.1.0";
+                  sha256 = "sha256-TTYBaJ4gcMVICz4bGZTvbNRPpWD4tXuAJbI8QcHNDv0=";
+                }
                 {
                   name = "codegeex";
                   publisher = "aminer";
