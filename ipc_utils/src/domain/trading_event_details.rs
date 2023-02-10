a93632cb2502622f962c65935623f68d94ac3fbb
civts <giacomo.zanolli@studenti.unitn.it>
2023-02-10 19:11:46 +0100

    Merge trading event details enum 🎲

--- ipc_utils/src/domain/trading_event_details.rs
+++ ipc_utils/src/domain/trading_event_details.rs
@@ -2,22 +2,19 @@ use serde::{Deserialize, Serialize};
 use unitn_market_2022::good::good_kind::GoodKind;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub enum TradingEventDetails {
-    // TODO: we can merge these structs
-    AskedLock {
-        successful: bool,
-        trade_type: TradeType,
-        good_kind: GoodKind,
-        quantity: f32,
-        price: f32,
-    },
-    TradeFinalized {
-        successful: bool,
-        trade_type: TradeType,
-        good_kind: GoodKind,
-        quantity: f32,
-        price: f32,
-    },
+pub struct TradingEventDetails {
+    operation: TradeOperation,
+    successful: bool,
+    trade_type: TradeType,
+    good_kind: GoodKind,
+    quantity: f32,
+    price: f32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum TradeOperation {
+    AskedLock,
+    TradeFinalized,
 }
 
 #[derive(Serialize, Deserialize, Copy, Clone, Debug, PartialEq, Eq, Hash)]
